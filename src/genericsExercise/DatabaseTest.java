1a3c73abe87cc3879cdfbf9ec49b806ce077fb9d
kasia <katriina.was@gmail.com>
2024-01-02 08:43:36 +0100

    arraysMethods - exercises

---
+++ src/genericsExercise/DatabaseTest.java
@@ -0,0 +1,23 @@
+package genericsExercise;
+
+public class DatabaseTest {
+    public static void main(String[] args) {
+        PersonDatabase database = new PersonDatabase();
+        database.add(new Person("John", "Burton", "1234567"));
+        database.add(new Person("Kate", "Smith", "11223344"));
+        database.add(new Person("Sebastian", "Mueler", "3333333"));
+        database.add(new Person("Ann", "Anderson", "7367677"));
+        database.add(new Person("Walter", "Wonka", "468746578"));
+        database.add(new Person("Leo", "Holmes", "584876786"));
+
+        Person person = database.get(1);
+        System.out.println(person);
+        System.out.println(database);
+        database.remove(new Person("Ann", "Anderson", "7367677"));
+        System.out.println();
+        System.out.println(database);
+
+
+    }
+
+}
---
+++ src/genericsExercise/PersonDatabase.java
@@ -0,0 +1,60 @@
+package genericsExercise;
+
+import java.util.Arrays;
+
+public class PersonDatabase {
+    private final int INITIAL_CAPACITY = 1;
+    private Person[] persons = new Person[INITIAL_CAPACITY];
+    private int emptyPlace;
+
+    void add(Person person) {
+        if(person == null)
+            throw new NullPointerException("person cannot be null");
+        if(emptyPlace == persons.length){
+            persons = Arrays.copyOf(persons, persons.length * 2);
+        }
+        persons[emptyPlace] = person;
+        emptyPlace++;
+    }
+
+    void remove(Person person){
+        if(person == null)
+            throw new NullPointerException("person cannot be null");
+        final int notFound = -1;
+        int foundIndex = notFound;
+        for (int i = 0; i < emptyPlace && foundIndex ==notFound; i++) {
+            if(person.equals(persons[i])){
+                foundIndex = i;
+            }
+        }
+        if(foundIndex != notFound){
+            System.arraycopy(persons,foundIndex+1, persons, foundIndex, persons.length-foundIndex-1);
+            emptyPlace--;
+            persons[emptyPlace] = null;
+        }
+    }
+
+    Person get(int index){
+        if(index >= emptyPlace || emptyPlace < 0 )
+            throw new ArrayIndexOutOfBoundsException("Index has to be positive and lower than " + emptyPlace);
+        return persons[index];
+    }
+
+    int size(){
+        return  emptyPlace;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("PersonDatabase size: " + emptyPlace);
+        sb.append("\n");
+        for (int i = 0; i < emptyPlace; i++) {
+            sb.append(" > ");
+            sb.append(persons[i].toString());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
